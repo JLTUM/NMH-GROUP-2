c8624a795787531a4fc35b959861e473f482dd5e
zweihuehner <faro.schaefer@gmx.de>
2020-04-24 20:43:36 +0200

    kleine verbesserungen

--- one.m
+++ one.m
@@ -1,35 +1,37 @@
-%% Header
+%% ---------------------------- Header --------------------------------- %%
 
 %%% Numerische Methoden der Hydromechanik
 %%% Assignment: 1
 %%% Group: 2
 %%% Members: Nick Pfeiffer, Andreas Mirlach, Julian Lenz, Faro Schäfer
 
+% ----------------------------------------------------------------------- %
+
 clear all
 close all
 
 %% grid and analytical solution 
 
-grid = [10,20,50,100,1000,10000];
-% grid = linspace(10,10000,1000);
+grid = [10,20,50,100,1000,10000]; % amount of grid points 
+ 
 for j = 1:length(grid)
 
     % Number of grid points
     n = grid(j);
 
     % Grid spacing
-    h = 2.0*pi / (n-1);
+    h = 2.0 * pi / (n-1);
 
     % Calculation of... 
     for i = 1 : n
 
-      % ...coordinates
-      x(i) = 0.0 + (i-1) * h;
+        % ...coordinates
+        x(i) = 0.0 + (i-1) * h;
 
-      % ...analytical values of function and derivative
-      f(i) = sin(2*x(i)); % analytical
-      dfe(i) = cos( 2*x(i) ) * 2; % analytical first derivative
-      dfe2(i) = -sin( 2*x(i) ) * 4; % analytical second derivative
+        % ...analytical values of function and derivative
+        f(i) = sin(2*x(i)); % analytical function
+        dfe(i) = cos( 2*x(i) ) * 2; % analytical first derivative
+        dfe2(i) = -sin( 2*x(i) ) * 4; % analytical second derivative
       
     end
     
@@ -37,31 +39,31 @@ for j = 1:length(grid)
 
     for i = 1 : n
 
-      % Periodic boundary conditions... 
+        % Periodic boundary conditions... 
 
-      % ...for the first point
-      if ( i == 1 )
+        % ...for the first point
+        if ( i == 1 )
 
-        fp = f(i+1);
-        fm = f(n-1);
+            fp = f(i+1);
+            fm = f(n-1);
 
-      end
+        end
 
-      % ...for the last point
-      if ( i == n )
+        % ...for the last point
+        if ( i == n )
 
-        fp = f(2);
-        fm = f(n-1);
+            fp = f(2);
+            fm = f(n-1);
 
-      end
+        end
 
-      % If point is not at the boundaries 
-      if ( ( i ~= 1 ) && ( i ~= n ) )
-        fp = f(i+1);
-        fm = f(i-1);
-      end
+        % If point is not at the boundaries 
+        if ( ( i ~= 1 ) && ( i ~= n ) )
+            fp = f(i+1);
+            fm = f(i-1);
+        end
 
-      fi = f(i);
+        fi = f(i);
 
 %% function values 
 
@@ -90,17 +92,17 @@ for j = 1:length(grid)
         er_C2(i) = abs ( ( dfe2(i) - dfn_C2(i) ) / dfe(i) );
         
         
-    end  
-
+    end
+    
 %% Interpolation of Points
 
-    xq = [0+h/2:h:n];
-    dfn_U_interp = interp1(x,dfn_U,xq);
-    dfn_D_interp = interp1(x,dfn_D,xq);
-    dfn_C_interp = interp1(x,dfn_C,xq);
-    dfn_C2_interp = interp1(x,dfn_C2,xq);
+    xq = [0+h/2:h:n]; % x-values on which function values get interpolated: points in the middle of two grid points
+    dfn_U_interp = interp1(x,dfn_U,xq); % Interpolated Points for Upwind
+    dfn_D_interp = interp1(x,dfn_D,xq); % Interpolated Points for Downwind
+    dfn_C_interp = interp1(x,dfn_C,xq); % Interpolated Points for Central
+    dfn_C2_interp = interp1(x,dfn_C2,xq); % Interpolated Points for Central (second der.)
     
-    %% Plots
+%% Plots
     
     if j == 1 
         fig_first_derivative = figure;
@@ -110,7 +112,7 @@ for j = 1:length(grid)
     % plot first derivative 
     set(0,'CurrentFigure',fig_first_derivative)
     subplot(3,2,j)
-    plot(x, dfn_U, 'b.:', x, dfn_D, 'r.:', x, dfn_C, 'g.:', x, dfe, '--k') % numerical values
+    plot(x, dfn_U, 'b.:', x, dfn_D, 'r.:', x, dfn_C, 'g.:', x, dfe, '--k') % values of numerical difference scheme
     hold on 
     plot(xq, dfn_U_interp, 'b.', xq, dfn_D_interp, 'r.', xq, dfn_C_interp, 'g.') % interpolated values
     legend('Upwind','Downwind','Central', 'Exact')
@@ -120,14 +122,14 @@ for j = 1:length(grid)
     % plot second derivative
     set(0,'CurrentFigure',fig_second_derivative)
     subplot(3,2,j)
-    plot(x, dfn_C2, 'g.:', x, dfe2, '--k')
+    plot(x, dfn_C2, 'g.:', x, dfe2, '--k') % values of numerical difference scheme
     hold on
-    plot(xq, dfn_C2_interp, 'g.')
+    plot(xq, dfn_C2_interp, 'g.') % interpolated values
     legend('Central', 'Exact')
     set(gca,'FontSize',14); 
     title(n);
     
-    % Storing grid spacing and error in matrix 'error'
+    % Storing grid spacing and relative error in matrix
     error(j,1) = h;
     error(j,2) = er_U(n/5);
     error(j,3) = er_D(n/5);
@@ -135,7 +137,8 @@ for j = 1:length(grid)
     error(j,5) = er_C2(n/5);
     
 end
-% Plotting of error over grid spacing in normal scale
+
+% Plotting of error over grid spacing in log scale
 figure
     loglog(error(:,1),error(:,2),'-',error(:,1),error(:,3),'-',error(:,1),error(:,4),'-', x, x,'-k');
     title('Error plot first derivative');
@@ -143,7 +146,7 @@ figure
     ylabel('Relative error');
     legend('Error Upwind','Error Downwind','Error Central','Location','SouthEast')
 
-% Plotting of error over grid spacing in normal scale
+% Plotting of error over grid spacing in log scale
 figure
     loglog(error(:,1),error(:,5),'-', x, x,'-k');
     title('Error plot second derivative');
