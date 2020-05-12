731258f7dd5687eeec5c4038505e09efe4cd5df4
zweihuehner <faro.schaefer@gmx.de>
2020-05-12 20:42:33 +0200

    neuester stand

--- Assignment 2/A_D_eq.m
+++ Assignment 2/A_D_eq.m
@@ -1,7 +1,7 @@
 %% ---------------------------- Header --------------------------------- %%
 
 %%% Numerische Methoden der Hydromechanik
-%%% Assignment: 1
+%%% Assignment: 2
 %%% Group: 2
 %%% Members: Nick Pfeiffer, Andreas Mirlach, Julian Lenz, Faro Schäfer
 
@@ -18,17 +18,10 @@
 
 function A_D_eq(U0,Gamma,points,nn,scheme)
 
+global sp; % sp: subplot number 
 
 % Clear all variables and plots.
 format long;
-close all
-hold off;
-
-% Set convection velocity
-% U0 = [10];
-
-% Set diffusivity
-% Gamma = 1.0;
 
 % Discrete spacing in space!
 xend   = 2.0*pi;
@@ -56,55 +49,89 @@ phi_end = 1.0;
 for i = 2 : points-1
 
     if scheme == "Central"
+        
         % Central Sheme
         a_w = ((U0/2*dx)+(Gamma/dx^2));
         a_p = (2*Gamma)/dx^2;
         a_e = ((-U0/2*dx)+(Gamma/dx^2));
+        % assign values to matrix A
+        A(i,i-1) = a_w;
+        A(i,i) = a_p;
+        A(i,i+1) = a_e;
+    
+    elseif scheme == "Upwind"
         
-    elseif scheme == "Upwind";
-
         % Upwind
         a_w = ((U0/dx)+(Gamma/dx^2));
         a_p = ((-U0/dx)-((2*Gamma)/dx^2));
         a_e = (Gamma/dx^2);
+        % assign values to matrix A
+        A(i,i-1) = a_w;
+        A(i,i) = a_p;
+        A(i,i+1) = a_e;
+    
+    elseif scheme == "Both"
+        
+        % Upwind 
+        a_w_u = ((U0/dx)+(Gamma/dx^2));
+        a_p_u = ((-U0/dx)-((2*Gamma)/dx^2));
+        a_e_u = (Gamma/dx^2);
+        % Central Sheme
+        a_w_c = ((U0/2*dx)+(Gamma/dx^2));
+        a_p_c = (2*Gamma)/dx^2;
+        a_e_c = ((-U0/2*dx)+(Gamma/dx^2));
+        % assign values to matrix A
+        A(i,i-1,1) = a_w_u;
+        A(i,i,1) = a_p_u;
+        A(i,i+1,1) = a_e_u;
+        A(i,i-1,2) = a_w_c;
+        A(i,i,2) = a_p_c;
+        A(i,i+1,2) = a_e_c;
+        
     end
-    
-     
-    % assign values to matrix A
-    
-    A(i,i-1) = a_w;
-    A(i,i) = a_p;
-    A(i,i+1) = a_e;
-    
 end
 
 % Boundary conditions
 
 % at i = 1
- A(1,1) = 1;
+ A(1,1,:) = 1;
  b(1) = phi_0;
 
 % at i = points
- A(points,points) = 1;
+ A(points,points,:) = 1;
  b(points) = phi_end;
 
-% Solution of the linear system
-phi = A\b;
-
 % Analytical solution
-
 phi_analytic = (exp((U0.*x/Gamma))-1)/(exp((2*pi*U0)/Gamma)-1);
 
-% error
-
-% nn = points / 2;
-er = (phi_analytic(nn) - phi(nn)) / phi_analytic(nn);
+% Solution of the linear system
+if scheme == "Both"
+    phi(:,1) = A(:,:,1)\b;
+    phi(:,2) = A(:,:,2)\b;
+else
+    phi = A\b;
+end
 
 % Plot the solution
-plot(x,phi,'r', x,phi_analytic, 'gx');
+
+subplot(2,2,sp)
+if scheme == "Upwind"
+    plot(x,phi,'r', x,phi_analytic, 'xk');
+%     hold on
+elseif scheme == "Central"
+    plot(x,phi,'g', x,phi_analytic, 'xk');
+%     hold on
+elseif scheme == "Both"
+    plot(x,phi(:,1),'r',x,phi(:,2),'g',x,phi_analytic,'xk');
+%     hold on
+end
+
+% error
+% nn = points / 2;
+% er = (phi_analytic(nn) - phi(nn)) / phi_analytic(nn);
 
 % Plot the error
-% 
 % figure
 % loglog(points,er)
+sp = sp +1;
 end
--- Assignment 2/main_two.m
+++ Assignment 2/main_two.m
@@ -1,7 +1,31 @@
+close all
+global sp;
+sp = 1;
+
 U0 = 10;
 Gamma = 1;
-points = 40;
+points = 5;
 nn = points/2;
-scheme = "Central"; % "Central" or "Upwind"
+scheme = "Upwind"; % "Central" or "Upwind or "Both"
+A_D_eq(U0,Gamma,points,nn,scheme)
 
+U0 = -10;
+Gamma = 1;
+points = 5;
+nn = points/2;
+scheme = "Upwind"; % "Central" or "Upwind or "Both"
+A_D_eq(U0,Gamma,points,nn,scheme)
+
+U0 = 10;
+Gamma = 1;
+points = 51;
+nn = points/2;
+scheme = "Upwind"; % "Central" or "Upwind or "Both"
+A_D_eq(U0,Gamma,points,nn,scheme)
+
+U0 = -10;
+Gamma = 1;
+points = 51;
+nn = points/2;
+scheme = "Upwind"; % "Central" or "Upwind or "Both"
 A_D_eq(U0,Gamma,points,nn,scheme)
