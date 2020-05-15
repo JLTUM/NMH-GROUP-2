f9c7ea20bf775d8f69d9b89c83766511901e6e27
zweihuehner <faro.schaefer@gmx.de>
2020-05-15 15:04:35 +0200

    Code done

--- Assignment 3/A_D_FV.m
+++ Assignment 3/A_D_FV.m
@@ -4,29 +4,9 @@
 %%% Assignment: 3
 %%% Group: 2
 %%% Members: Nick Pfeiffer, Andreas Mirlach, Julian Lenz, Faro Schäfer
-
-% ----------------------------------------------------------------------- %
-
-% This is a Matlab code to solve 1D steady advection-diffusion equation
-% discritised by finite-volume schemes 
-%
-% differential form of advection-diffusion eqn.:
-%
-%       d phi          d^2 phi
-%  -U0 ------- + Gamma ------- = 0
-%        dx             dx^2
-%
-% You are asked to fill in the missing parts to complete the implementation.
-% Missing parts are marked by ???
-%
-% Author: Yoshiyuki Sakai
-% Email: yoshiyuki.sakai@tum.de
-
 %% 
 
-function A_D_FV(U0,Gamma,cells)
-
-global sp; % sp: subplot number 
+function [x, phi, x_analytic, phi_analytic, err_rel, err_mean] = A_D_FV(U0,Gamma,cells)
 
 % Clear all variables and plots.
 format long;
@@ -34,7 +14,7 @@ format long;
 % Set up grid cells
 xend = 2.0 * pi;
 dx = xend/(cells); % dx for cells
-dx_analytic = xend/(1000); % dx for analytic 
+dx_analytic = xend/(1001); % dx for analytic 
 
 % Array of grid cell centre locations:
 x = dx/2:dx:xend-(dx/2);
@@ -82,7 +62,7 @@ end
 
 % Solution of the linear system
 phi = A\b;
-phi = [phi_0; phi; phi_end]; % include boundary conditions 
+phi = [phi_0; phi; phi_end]'; % include boundary conditions 
 x = [0 x xend]; % include boundary conditions 
 
 % Compute analytical solution
@@ -91,52 +71,11 @@ phi_analytic = (exp((U0.*x_analytic/Gamma))-1)/(exp((2*pi*U0)/Gamma)-1);
 phi_ex = (exp((U0.*x/Gamma))-1)/(exp((2*pi*U0)/Gamma)-1);
 
 % Compute relative error
- nn = ceil(cells / 2)+1; % value at pi
- err_rel = (phi_analytic(nn) - phi(nn)) / phi_analytic(nn);
- err_mean = sqrt(mean((phi_ex' - phi).^2))/mean(phi_ex);
-
-%h=figure
-subplot(3,2,sp)
-hold on
-plot(x, phi, '-.r', x_analytic, phi_analytic, '--k');
-legend('Numerical','Analytic')
-if sp == 1
-title('U: 1 / cells: 51');
-elseif sp == 2
-title('U: -1 / cells: 51');
-elseif sp == 3
-title('U: 10 / cells: 5');
-elseif sp ==4
-title('U: -10 / cells: 5');
-elseif sp == 5
-title('U: 10 / cells: 51');
-elseif sp == 6
-title('U: -10 / cells: 51');
-end
-
-%saveas(h,sprintf('FIG%d.png',sp));
-%hold on 
-
-% if sp == 1
-%     fig_A_D_eq = figure('units','normalized','outerposition',[0 0 1 1]);
-% end
-
-%set(0,'CurrentFigure',fig_A_D_eq)
-% subplot(3,3,sp)
-% hold on
-% if sp == 1
-% title('U: 10 Points: 5');
-% elseif sp == 2
-% title('U: -10 Points: 5');
-% elseif sp == 3
-% title('U: 10 Points: 51');
-% elseif sp == 4
-% title('U: -10 Points: 51');
-% end
-
-sp = sp +1;
-
-% Plot the error as function of dx in log-log scale
-% ???
-
+ nn = ceil(cells / 3 ) + 1; % value at pi
+ err_rel = abs((phi_ex(nn) - phi(nn)) / phi_ex(nn));
+ err_mean = sqrt(mean((phi_ex - phi).^2))/mean(phi_ex);
+ 
+%  err_rel = abs((phi_ex - phi) ./ phi_ex);
+%  plot(x,err_rel)
+ 
 end
