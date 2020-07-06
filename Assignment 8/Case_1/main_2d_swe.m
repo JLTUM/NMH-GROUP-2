694343e888f1f99f7d7172070dfa00c87fe11d53
zweihuehner <faro.schaefer@gmx.de>
2020-07-06 15:15:58 +0200

    adad

--- Assignment 8/Case_1/main_2d_swe.m
+++ Assignment 8/Case_1/main_2d_swe.m
@@ -11,6 +11,7 @@ close all
 
 %% Initialize simulation
 % read infile 
+global infilename
 infilename = 'infile_2D_swe_channelFlow2.mat'; %% 1,2,3,4,5
 fprintf('infilename is: %s\n', infilename)
 
@@ -32,123 +33,93 @@ run.t = 0;
 
 [ flow ] = set_initial_condition( grid, flow);
 
-
-
-% ---- Setup of for time integration --------------------------------------
-% Frequency of diagnostic output
-%itdiag = 100;
-
 %% Preallocation of variables
 b = grid.ymax;
 Q = bconds.huwest*b;
 
-
-%% Time integration
-
-N_M = NWV_muster(Q,b,0,-flow.I_s,30);
-
-%flow.h(:) = 1%N_M(1);
-%flow.hu(:) = 0%N_M(3)*N_M(1);
 I_WSP = [];
 I_S = [];
 I_E = [];
+
+N_M = NWV_muster(Q,b,0,-flow.I_S,30);
+
+I = -flow.I_S;
+k_st = flow.kst(1,1);
+N_M = NWV_muster(Q,b,0,I,k_st);
+
+%% Preallocation of Plots
+
+    fig_Hy = figure;
+    fig_Channeld = figure;
+        title('H-y Diagram')
+        y = 0:0.01:5;
+        hold on
+        H = y + ( Q )^2 ./ (b * y ).^2 ./ 2 ./ 9.81;
+        plot(H,y);
+        H = y;
+        y_c = (((Q/b)^2)/09.81)^(1/3);
+        yline(y_c);
+        plot(H,y);
+        xlim([0 5])
+        xlabel('H') 
+        ylabel('y')
+
+%% Time integration
+
 for itstep = 1:run.ntst
     [ run, flow ] = time_step_rk( itstep==1, constants, grid, run, ...
         flow, bconds );
     
-    I = -flow.I_s;
-    k_st = flow.kst(1,1);
-    N_M = NWV_muster(Q,b,0,I,k_st);
-    v_h = flow.hu(:,2) ./ flow.h(:,2);
+    % Berechnung NWV
+    u = flow.hu(:,2) ./ flow.h(:,2);
     A = flow.h(:,2) .* grid.ymax;
     U = 2 .* flow.h(:,2) + grid.ymax;
     R = A ./ U;
-    v_st = k_st*sqrt(I)*R.^(2/3);
+    v_st = k_st*sqrt(I) * R.^(2/3);
     
-    Fr = v_h./(flow.h(:,2).*9.81).^(0.5);
-    energy = v_h.^(2)/9.81/2+flow.h(:,2)+flow.zb(:,2);
+    % Berechnung Fr und H
+    Fr = u ./ (flow.h(:,2).*9.81).^(0.5);
+    H = u.^(2)/9.81/2 + flow.h(:,2) + flow.zb(:,2);
     
+    % Berechnung Gefällelinien
+    I_WSP(end+1) = abs((flow.zb(2,2) + flow.h(2,2)) - ((flow.zb(end,2) + flow.h(end,2))) )  / grid.xmax;
+    I_S(end+1) = abs((flow.zb(2,2)-flow.zb(end))) / grid.xmax;
+    I_E(end+1) = abs(H(2) - H(end)) / grid.xmax;
     
+%% Plot Results
     
-
-    I_WSP(end+1) = abs((flow.zb(grid.NGHOST+1,grid.NGHOST+1) + flow.h(grid.NGHOST+1,grid.NGHOST+1)) -...
-        ((flow.zb(end,grid.NGHOST+1) + flow.h(end,grid.NGHOST+1))) )  / grid.xmax;
-    
-    I_S(end+1) = abs((flow.zb(grid.NGHOST+1,grid.NGHOST+1)-flow.zb(end))) / grid.xmax;
-   
-    I_E(end+1) = abs(energy(grid.NGHOST+1) - energy(end)) / grid.xmax;
-    
-
-    %% Calculate NWV   
-    
-
-    
-    figure(1)
-    
+    % Plot Channel Diagnosis
+    set(0, 'CurrentFigure', fig_Channeld)
     subplot(2,2,1)
-    plot(grid.x(2:end),v_st(2:end),grid.x(2:end),v_h(2:end));
+    plot(grid.x(2:end),v_st(2:end),grid.x(2:end),u(2:end));
     title('Channel Velocity')
  
     subplot(2,2,2)
+    hold on
     plot(1:itstep,I_WSP,1:itstep,I_S,1:itstep,I_E)
+    hold off
     title('Channel I_WSP')
     
     subplot(2,2,3)
-    plot(grid.x,flow.h(:,2)+flow.zb(:,2),grid.x,energy,grid.x,flow.hu,grid.x,flow.zb,'b');
+    plot(grid.x,flow.h(:,2)+flow.zb(:,2),grid.x,H,grid.x,flow.hu,grid.x,flow.zb,'b');
     title('Channel Waterdepth / Energy / Discharge')
     legend('h','H','q','zb','Location','southwest')
     
+    hold off
     subplot(2,2,4)
     plot(grid.x,Fr);
     title('Channel Froude')
     legend('Fr','Location','northwest')
+
   
-    figure(2)
-    title('H-y Diagram')
-    y = 0:0.01:5;
-    %H = zeros(1,91);
+    % Plot Hy-Diagramm
+    set(0, 'CurrentFigure', fig_Hy)
     hold on
-    H = y + ( Q )^2 ./ (b * y ).^2 ./ 2 ./ 9.81;
-    plot(H,y);
-    H = y;
-    y_c = (((Q/b)^2)/09.81)^(1/3);
-    yline(y_c);
-    plot(H,y);
-    xlim([0 5])
-    xlabel('H') 
-    ylabel('y')
-    scatter(flow.h(90,2),energy(90))
-    scatter(flow.h(20,2),energy(20))
-    scatter(N_M(1),N_M(2),'b')
+    scatter(flow.h(90,2),H(90),'b')
+    scatter(flow.h(20,2),H(20),'r')
+    scatter(N_M(1),N_M(2),'y')
     hold off
-    
-    
- 
-
-%% Plot Results
-
-%     v_n(end+1)=N_M(3);
-%     % Channel Diagnosis
-%     figure(1);
-%     subplot(2,2,1);
-%     plot(1:itstep,I_S,'-b', 1:itstep,I_WSP,'-r', 1:itstep, v_st,'-g')
-%     legend('I_S','I_{WSP}','v_{st}','Location','northwest')
-%     subplot(2,2,2);
-%     plot(grid.x,flow.h(:,2)+flow.zb(:,2))
-%     title('Channel diagnosis')
-%     
-%     subplot(2,2,3);
-%     plot(grid.x,flow.hu(:,2)./ flow.h(:,2))
-%     yline(N_M(3))
-%     title('Channel Velocity Test')
-%     
-%     subplot(2,2,4);
-%     energy = ( flow.hu(:,2)./flow.h(:,2) ).^2 / (2*9.81) + flow.h(:,2);
-%     set(0, 'CurrentFigure', fig_hu)
-%     plot(grid.x,energy)
-%     yline(N_M(2))
-%     title('Channel Energy Test')
-%     
+      
     pause(0.0001)
     
 
